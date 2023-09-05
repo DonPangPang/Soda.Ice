3457ad00bd889929ed8871e93d6a01230a461c53
Powers <powertle@outlook.com>
2023-09-05 11:24:43 +0800

    添加markdown支持

---
+++ src/Soda.Ice.Common/Extensions/MarkdownExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Markdig;
+
+namespace Soda.Ice.Common;
+
+public static class MarkdownExtensions
+{
+    public static string GetContent(this string markdown)
+    {
+        var doc = Markdown.Parse(markdown);
+        foreach (var block in doc.AsEnumerable())
+        {
+            if (block is Markdig.Syntax.ParagraphBlock)
+            {
+                Console.WriteLine(block.ToString());
+            }
+        }
+
+        return string.Join("", doc.AsEnumerable().Where(x=>x is Markdig.Syntax.ParagraphBlock).Select(x=>x.ToString()));
+    }
+}
